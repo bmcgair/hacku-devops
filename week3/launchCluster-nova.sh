b36e2cde12be1d41cdf41aa196b61e5da8e04419
Bill McGair <bill@mcgair.com>
2016-02-29 18:08:59 -0800

    readme updates

--- week3/launchCluster-nova.sh
+++ week3/launchCluster-nova.sh
@@ -4,15 +4,16 @@
 #### Increment BASTION_ID to launch new box!  ####
 ### Otherwise script will behave idempotently ####
 ##################################################
-export BASTION_IDEMPOTENT_ID="05"
+export BASTION_IDEMPOTENT_ID="79"
+
 export PROVISION_STATE="present"
 
-export AWS_REGION="us-west-2"
-export ZONEA="us-west-2a"
-export ZONEB="us-west-2b"
-export ZONEC="us-west-2c"
+export AWS_REGION="us-east-1"
+export ZONEA="us-east-1a"
+export ZONEB="us-east-1b"
+export ZONEC="us-east-1e"
 
-export VPC_BASE="10.10"
+export VPC_BASE="10.20"
 export KEYPAIR="xaas-or"
 export MGMT_IP="`curl -s icanhazip.com`/32"
 
